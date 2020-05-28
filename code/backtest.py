29f9eb7b2b67b1fc69f2e3f0e0b072644d41e474
44050496 <ra.redmer@outlook.com>
2020-05-28 12:17:30 +0200

    Extended strategy by short- an multiple position

--- code/backtest.py
+++ code/backtest.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from sklearn.model_selection import PredefinedSplit
 from scipy.stats.mstats import gmean
 from copy import deepcopy
+from dataclasses import dataclass
 
 #%%      
 class Backtest:
@@ -42,7 +43,7 @@ class Backtest:
             self.trading_signals[ f"max_{column}" ] = self.trading_signals[ f"{column}" ].max(axis=1)
             self.trading_signals[ f"max_{column}_pair" ] = self.trading_signals[ f"{column}" ].idxmax(axis=1)
 
-        # self.trading_signals = self.trading_signals.head(2400) #### REMOVE
+        self.trading_signals = self.trading_signals.head(400) #### REMOVE
 
     def equal_weight_holding_return(self, transaction_cost=0.003):
         pairs = self.test_df.index.get_level_values("pair").unique().values
@@ -57,7 +58,7 @@ class Backtest:
         data["equal_weight"] = [ equal_weight_long_return, equal_weight_short_return, mean_equal_weight_return ]
         return pd.DataFrame.from_dict(data, orient="index", columns=[ "long_return", "short_return", "mean_return" ]) - transaction_cost
                    
-    def conduct_top_short_long_alg(self, threshold=0, positions=120, transaction_cost=0.003, delta=120, skip_zero_volume=True):
+    def conduct_top_short_long_alg(self, threshold=0, positions=120, transaction_cost=0.003, delta=120, position_type="both", skip_zero_volume=True):
         """ Simulate trading on the submitted trading data and generated trading-signals by the respective model.
             DISCLAIMER: As of right now, only single long position.
             (outlined in more detail in chapter ???)
@@ -77,7 +78,7 @@ class Backtest:
                 pd.DataFrame: Returns for each trade made for each position
         """        
         ts = TopLongShortStrategy(trading_signals=self.trading_signals, positions=positions, delta=delta)
-        position_return_df =  ts.conduct_strategy(threshold, skip_zero_volume, transaction_cost)
+        position_return_df =  ts.conduct_strategy(threshold, skip_zero_volume, transaction_cost, position_type=position_type)
         return position_return_df
 
 
@@ -90,31 +91,29 @@ class TopLongShortStrategy:
         self.delta = delta
         self._start_row_num = 0
         self.decision_function = self._top_2state_decision if self.states == 2 else self._top_3state_decision
+        self._return_position = {}
 
-    def _top_2state_decision(self, max_up_prob_pair="", max_down_prob_pair="", threshold=1/2):
-        # if max_up_prob_pair and max_down_prob_pair:
-        #     raise ValueError
-        # print( row["max_up_prob"], row["stable_prob"][ max_up_prob_pair ][0] )
-        for max_pair in [max_up_prob_pair, max_down_prob_pair]:
-            if max_pair:  
-                max_prob = self._bin[f"up_prob"][ max_pair ][0]
-                if max_prob > threshold:
-                    return True
-                else:
-                    return False   
+    def _top_2state_decision(self, pair, threshold=1/2):
+        prob_col = "up_prob" if self._position_state["type"] == "long" else "down_prob"
+        max_prob = self._bin[ prob_col ][ pair ][0]
+        if max_prob > threshold:
+            return True
+        else:
+            return False   
 
-    def _top_3state_decision(self, max_up_prob_pair="", max_down_prob_pair="", threshold=1/3):
-        for movement, max_pair in zip( ["up", "down"], [max_up_prob_pair, max_down_prob_pair] ):
-            if max_pair:
-                pair_stable_prob = self._bin["stable_prob"][ max_pair ][0] 
-                other_movement = "up" if movement == "down" else "down"
-                other_movement_prob = self._bin[f"{other_movement}_prob"][ max_pair ][0] 
-                if self._bin[f"max_{movement}_prob"][0] > np.nanmax( [ threshold, pair_stable_prob, other_movement_prob ] ):
-                    return True
-                else:
-                    return False
+    def _top_3state_decision(self, pair, threshold=1/3):
+        movement = "up" if self._position_state["type"] == "long" else "down"
+        pair_stable_prob = self._bin["stable_prob"][ pair ][0] 
+        other_movement = "up" if movement == "down" else "down"
+        other_movement_prob = self._bin[f"{other_movement}_prob"][ pair ][0] 
+        if self._bin[f"max_{movement}_prob"][0] > np.nanmax( [ threshold, pair_stable_prob, other_movement_prob ] ):
+            return True
+        else:
+            return False
 
-    def _get_return(self, position_type):
+    def _get_return(self):
+        """ Calculate return of current position depending on the type of position ie long or short
+        """
         return_factor = 1 if self._position_state["type"] == "long" else -1
         return (
             (self._position_state["close_price"] - self._position_state["open_price"]) * return_factor
@@ -123,13 +122,15 @@ class TopLongShortStrategy:
         )
 
     def _close_position(self):
+        """ Calculate return of current position and then close it ie reset the position's attributes  """
         # skip min-bins until volume is not zero
         self._skip_to_non_zero_volume_row(self._position_state["pair"])
 
         self._position_state["close_price"] = self._bin["middle_median"][ self._position_state["pair"] ][0]
-        self._position_state["close_return"] = self._get_return(position_type="long")
+        self._position_state["close_return"] = self._get_return()
         self._position_state["close_time"] = self._bin.index[0]
-        self._position_state["duration"] = self._row_num - self._position_state["open_row_num"]
+        self._position_state["close_row_num"] = self._row_num
+        self._position_state["duration"] = self._position_state["close_row_num"] - self._position_state["open_row_num"]
         # add closed position info to list
         self._total_position_data.append( deepcopy(self._position_state) )
         # reset position
@@ -160,9 +161,9 @@ class TopLongShortStrategy:
         # update position opening
         self._position_state["open_time"] = self._bin.index[0]
 
-    def _open_position_under_constraint(self, new_pair, execution_gap=1):
+    def _open_position_under_constraint(self, new_pair):
         # go to next min-bin to enter new position (execution gap)
-        self._row_num += execution_gap
+        self._row_num += self._execution_gap
         self._bin = self.trading_signals.iloc[ [ self._row_num ] ]
         if self._skip_zero_volume:
             volume_max_pair = self._bin["volume_scaled"][ new_pair ][0]
@@ -173,89 +174,154 @@ class TopLongShortStrategy:
         else:
             self._open_position(new_pair=new_pair)
 
-    def conduct_strategy(self, threshold=0, skip_zero_volume=False, transaction_cost=0.003, execution_gap=1):
+    def conduct_strategy(self, threshold=0, skip_zero_volume=False, transaction_cost=0.003, execution_gap=1, position_type="both"):
         threshold = 1 / self.states if not threshold else threshold
 
-        self._start_row_num = 0 if not self._start_row_num else self._start_row_num
+        
         self._total_position_data = []
         self._transaction_cost = transaction_cost
         self._skip_zero_volume = skip_zero_volume
-        # inside loop through positions
-        position_num = 1
-        print("Position:", position_num)
-        self._position_data = []
+        self._execution_gap = execution_gap
+        types = {
+            "long": ["long"],
+            "short": ["short"],
+            "both": ["short", "long"],
+        }
+        for _type in types[ position_type ]:
+            self._start_row_num = 0
+            movement = "up" if _type == "long" else "down"
+            for position_num in range(1, self.positions + 1):
+                print("Position:", position_num, "Type:", _type)
+                # self._start_row_num = 0 if not self._start_row_num else self._start_row_num
+                self._position_state = dict(
+                    position_num=position_num,
+                    type=_type,
+                    pair = "",
+                    open_row_num="",
+                    open_time = "",
+                    open_price = "",
+                    duration = "",
+                    close_row_num="",
+                    close_time="",         
+                    close_price = "",
+                    close_return = "",
+                )
+                self._prob_signal = False
+                # determine start_row_num, last_price and last_pair
+                self._row_num = self._start_row_num + 1
+                if self._skip_zero_volume:
+                    while self._row_num < self._max_row_num and not self._position_state["pair"]:
+                        # update bin
+                        self._bin = self.trading_signals.iloc[ [ self._row_num ] ]
+                        # get pair for which hast the max up-prob
+                        max_prob_pair = self._bin[f"max_{movement}_prob_pair"][0]
+                        # determine whether the pair fulfills prob-conditions
+                        self._prob_signal = self.decision_function(pair=max_prob_pair, threshold=threshold)
+                        # open new position next bin, if any volume is traded
+                        self._open_position_under_constraint(max_prob_pair)
+                else:
+                    # update bin
+                    self._bin = self.trading_signals.iloc[ [ self._row_num ] ]
+                    # get pair for which hast the max up-prob
+                    max_prob_pair = self._bin[f"max_{movement}_prob_pair"][0]
+                    # open position
+                    self._open_position(max_prob_pair)
+                # save start row
+                self._start_row_num = self._row_num          
 
-        self._position_state = dict(
-            position_num=position_num,
-            type="long",
-            pair = "",
-            open_row_num="",
-            open_time = "",
-            open_price = "",
-            duration = "",
-            close_time="",         
-            close_price = "",
-            close_return = "",
-        )
-        self._prob_signal = False
-        # determine start_row_num, last_price and last_pair
-        self._row_num = self._start_row_num + 1
-        if self._skip_zero_volume:
-            while self._row_num < self._max_row_num and not self._position_state["pair"]:
-                # update bin
-                self._bin = self.trading_signals.iloc[ [ self._row_num ] ]
-                # get pair for which hast the max up-prob
-                max_prob_pair = self._bin["max_up_prob_pair"][0]
-                # determine whether the pair fulfills prob-conditions
-                self._prob_signal = self.decision_function(max_up_prob_pair=max_prob_pair, threshold=threshold)
-                # open new position next bin, if any volume is traded
-                self._open_position_under_constraint(max_prob_pair, execution_gap=execution_gap)
-        else:
-            # update bin
-            self._bin = self.trading_signals.iloc[ [ self._row_num ] ]
-            # get pair for which hast the max up-prob
-            max_prob_pair = self._bin["max_up_prob_pair"][0]
-            # open position
-            self._open_position(max_prob_pair)
-        # save start row
-        self._start_row_num = self._row_num          
-
-        print(f'Postion: {position_num}, Start-datetime: {self._position_state["open_time"]} (row number: {self._start_row_num})')
-        # skip next delta min-bins
-        self._row_num += self.delta
-        while self._row_num < self._max_row_num - self.delta:
-            # get current volumes and prob
-            self._bin = self.trading_signals.iloc[ [ self._row_num ] ]
-            # get pair which has the max up-prob
-            max_prob_pair = self._bin["max_up_prob_pair"][0]            
-            # determine whether the pair fulfills prob-conditions
-            self._prob_signal = self.decision_function(max_up_prob_pair=max_prob_pair, threshold=threshold)
-            # check if prob-condition is fulfilled 
-            if self._prob_signal:
-                # if not active position, but good prob-signal, then open new position
-                if not self._position_state["pair"]:
-                    # open new position next bin, if any volume is traded
-                    self._open_position_under_constraint(max_prob_pair, execution_gap=execution_gap)
-
-                # if active position's pair is not the same as the suggested position by prob-signal, then close position
-                elif max_prob_pair != self._position_state["pair"]:
-                    # close position
-                    self._close_position()          
-                              
-            else:
-                # if active position, but bad prob-signal, then close position
-                if self._position_state["pair"]:
-                    # if not skip_zero_volume
-                    # close position
-                    self._close_position()
-                    
-            if self._position_state["pair"]:
-                # skip next delta minute-bins in all cases to contemplate new trading decision, thus remaining in active position
+                print(f'Postion: {position_num}, Start-datetime: {self._position_state["open_time"]} (row number: {self._start_row_num})')
+                # skip next delta min-bins
                 self._row_num += self.delta
-            else:
-                self._row_num += 1                 
+                while self._row_num < self._max_row_num - self.delta:
+                    # get current volumes and prob
+                    self._bin = self.trading_signals.iloc[ [ self._row_num ] ]
+                    # get pair which has the max up-prob
+                    max_prob_pair = self._bin[f"max_{movement}_prob_pair"][0]            
+                    # determine whether the pair fulfills prob-conditions
+                    self._prob_signal = self.decision_function(pair=max_prob_pair, threshold=threshold)
+                    # check if prob-condition is fulfilled 
+                    if self._prob_signal:
+                        # if not active position, but good prob-signal, then open new position
+                        if not self._position_state["pair"]:
+                            # open new position next bin, if any volume is traded
+                            self._open_position_under_constraint(max_prob_pair)
+
+                        # if active position's pair is not the same as the suggested position by prob-signal, then close position
+                        elif max_prob_pair != self._position_state["pair"]:
+                            # close position
+                            self._close_position()
+                                    
+                    else:
+                        # if active position, but bad prob-signal, then close position
+                        if self._position_state["pair"]:
+                            # if not skip_zero_volume
+                            # close position
+                            self._close_position()
+                            
+                    if self._position_state["pair"]:
+                        # skip next delta minute-bins in all cases to contemplate new trading decision, thus remaining in active position
+                        self._row_num += self.delta
+                    else:
+                        self._row_num += 1
+                # if a position is still active, close it
+                if self._position_state["pair"]:
+                    self._close_position()
+
         return pd.DataFrame(self._total_position_data)
     
+
+@dataclass    
+class Position:
+    position_num: int
+    type: str
+    pair: str = np.NaN
+    transaction_cost: int = 0.003
+    open_row_num: int = np.NaN
+    open_time: datetime = np.NaN
+    open_price: float = np.NaN
+    duration: int = np.NaN
+    close_time: datetime = np.NaN
+    close_price: float = np.NaN
+    close_return: float = np.NaN
+
+    def open(self, bin, pair):
+        pass
+
+    def _get_return(self):
+        """ Calculate return of current position depending on the type of position ie long or short
+        """
+        return_factor = 1 if self.type == "long" else -1
+        return (
+            (self.close_price - self.open_price) * return_factor
+            / self.open_price
+            - self.transaction_cost
+        )
+
+
+    @classmethod
+    def reset(cls):
+        _position_num = deepcopy( cls.position_num )
+        _type = deepcopy( cls.type )
+        cls.instance = None        
+
+        cls.instance = Position(position_num = _position_num, type = _type)
+
+    @classmethod
+    def close(cls, _bin):
+        """ Calculate return of current position and then close it ie reset the position's attributes  """
+        # skip min-bins until volume is not zero
+        # self._skip_to_non_zero_volume_row(self._position_state["pair"])
+
+        cls.instance.close_price = _bin["middle_median"][ cls.pair ][0]
+        cls.instance.close_return = cls.instance._get_return()
+        cls.instance.close_time = _bin.index[0]
+        cls.instance.duration = pd.Timedelta(cls.open_time - cls.close_time).seconds / 60.0
+
+        try:
+            return cls.instance.asdict()
+        finally:
+            cls.reset()
+
 
 #%%
 ##### TEST ######
@@ -278,7 +344,7 @@ test_data_df = top10_1min_df[
 #%%
 # create test and training data
 target = "future_2state_movement_120min"
-return_colum = "future_middle_return_120min"
+return_colum = "future_return_120min_constraint"
 x_columns = [ col for col in  top10_1min_df.columns if "return" in col and "future" not in col ]
 # test data from 2019-11-01 to 2019-12-31
 # x_test = top10_1min_df[ (top10_1min_df.index.get_level_values("time") >= "2019-11-01") ][x_columns]
@@ -299,7 +365,13 @@ bt = Backtest(
     return_column=return_colum,
 )
 #%%
-trading_decisions_df = bt.conduct_top_short_long_alg(threshold=1/2, positions=120, skip_zero_volume=True)
+trading_decisions_df = bt.conduct_top_short_long_alg(
+    threshold=1/2,
+    positions=3,
+    skip_zero_volume=True,
+    position_type="both",
+    transaction_cost=0.003,
+)
 
 #%%
 equal_weight_return_df = bt.equal_weight_holding_return()
